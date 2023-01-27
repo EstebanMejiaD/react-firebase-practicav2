ebc37ebb7a8b9ec192124f23405a595d51714abc
Esteban Mejia <estebandeveloper10@gmail.com>
2023-01-27 17:58:40 -0500

    Este es la rpactica final del crud react firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,157 +1,144 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import { firebase } from "./firebase";
+import {firebase} from './firebase'
+
 function App() {
-  const [listUser, setListUser] = useState([]);
-  const [nombre, setNombre] = useState("");
-  const [apellido, setApellido] = useState("");
-  const [id, setId] = useState("");
-  const [moodEdit, setMoodEdit] = useState(false);
-  const database = firebase.firestore();
+    const db = firebase.firestore()
+
+    //hooks
+    const [listUser, setListUser] = useState([])
+    const [nombre, setNombre] = useState("")
+    const [apellido, setApellido] = useState("")
+    const [id, setId] = useState("")
+    const [moodEdit, setMoodEdit] = useState(false)
+
   useEffect(() => {
-    const getData = async () => {
-      try {
-        const data = await database.collection("usuarios").get();
-        const arrayData = data.docs.map((document) => ({
+    const getData = async ()=> {
+       try {
+        const data = await db.collection("usuarios").get()
+        const arrayData = data.docs.map((document)=> ({
           id: document.id,
-          ...document.data(),
-        }));
-        setListUser(arrayData);
-      } catch (error) {
-        console.log(error);
-      }
-    };
+          ...document.data()
+        }))
+        setListUser(arrayData)
+       } catch (error) {
+        console.log(error)
+       }
+    }
 
-    getData();
-  }, []);
+      getData()
+  }, [])
+  
 
-  const saveData = async (e) => {
-    e.preventDefault();
-
+  const saveData = async (e)=> {
+    e.preventDefault()
     if (!nombre.trim()) {
-      alert("Ingrese un nombre");
-      return;
+      alert("Ingrese nombre")
+      return 
     }
     if (!apellido.trim()) {
-      alert("Ingrese un apellido");
-      return;
+      alert("Ingrese apellido")
+      return 
     }
 
     try {
+
       const newUser = {
         nombre,
-        apellido,
-      };
-
-      const data = await database.collection("usuarios").add(newUser);
+        apellido
+      }
+      const data = await db.collection("usuarios").add(newUser)
       setListUser([
         ...listUser,
         {
           id: data.id,
-          ...newUser,
-        },
-      ]);
+          ...newUser
+        }
+      ])
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
 
-    setApellido("")
     setNombre("")
-  };
+    setApellido("")
+  }
+
 
   const deleteData = async (id)=> {
-    try {
-      await database.collection("usuarios").doc(id).delete()
+      try {
 
-    const filterData = listUser.filter((user)=> user.id!==id)
-    setListUser(filterData)
-    } catch (error) {
-      console.log(error)
-    }
-
+        await db.collection("usuarios").doc(id).delete()
+        const filterListUser = listUser.filter((user)=> user.id!==id)
+        setListUser(filterListUser)
+      } catch (error) {
+        console.log(error)
+      }
   }
 
-  const modoEditing = (user)=> {
-    setNombre(user.nombre)
-    setApellido(user.apellido)
-    setId(user.id)
-    setMoodEdit(true)
-  }
+    const ismodeEdit=({nombre, apellido, id})=> {
+      setNombre(nombre)
+      setApellido(apellido)
+      setId(id)
+      setMoodEdit(true)
 
-  const editingData = async (e)=> {
-    e.preventDefault()
-
-    if (!nombre.trim()) {
-      alert("Ingrese un nombre");
-      return;
     }
-    if (!apellido.trim()) {
-      alert("Ingrese un apellido");
-      return;
+
+    const editData = async (e)=> {
+      e.preventDefault()
+      if (!nombre.trim()) {
+        alert("Ingrese nombre")
+        return 
+      }
+      if (!apellido.trim()) {
+        alert("Ingrese apellido")
+        return 
+      }
+
+      try {
+        const data = await db.collection("usuarios").doc(id).update({nombre,apellido})
+
+        const isEditedList = listUser.map((user)=>user.id === id ? {id, nombre,apellido} : user)
+
+        setListUser(isEditedList)
+
+        setApellido("")
+        setNombre("")
+        setId("")
+        setMoodEdit(false)
+      } catch (error) {
+        console.log(error)
+      }
+
     }
-    try {
-      await database.collection("usuarios").doc(id).update({
-        nombre,apellido
-      })
-
-      const updateList = listUser.map((user)=> user.id === id ? {
-        id: id,
-        nombre,
-        apellido
-      }: user) 
-      setListUser(updateList)
-
-      setNombre("")
-      setApellido("")
-      setId("")
-      setMoodEdit(false)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
   return (
-    <div className="container">
-      <form onSubmit={moodEdit ? editingData : saveData}>
-        <div>
-          <h2 className="text-center">{moodEdit ? "Editar usuario" : "Formulario de registro"}</h2>
-        </div>
-        <input
-          type="text"
-          placeholder="Ingrese nombre"
-          className="form-control my-2"
-          onChange={(e) => setNombre(e.target.value)}
-          value={nombre}
-        />
-        <input
-          type="text"
-          placeholder="Ingrese apellido"
-          className="form-control my-2"
-          onChange={(e) => setApellido(e.target.value)}
-          value={apellido}
-        />
-        <div className="d-grid gap-2 my-2">
-          {
-            moodEdit ? <button className="btn btn-warning">Editar</button> :
-            <button className="btn btn-success">Registrar</button>
-          }
-          
-        </div>
+    <div className="container ">
+      <form onSubmit={moodEdit ? editData : saveData}>
+          <div>
+            <h2>{moodEdit ? "Editar Usuario": "Registro de usuarios"}</h2>
+          </div>
+
+            <input type="text" className="form-control my-2" onChange={(e)=> setNombre(e.target.value)} value={nombre} placeholder="Ingrese nombre"/>
+            <input type="text" className="form-control  my-2" onChange={(e)=> setApellido(e.target.value)} value={apellido} placeholder="Ingrese apellido"/>
+            <div className="d-grid gap-2  my-2">
+            {
+              moodEdit ? <button className="btn btn-warning">Editar</button> : <button className="btn btn-success">Registar</button>
+            }
+            </div>
       </form>
-      <div className="render-list my-4">
-        <div>
-          <h2 className="text-center my-4">Lista de usuarios registrados</h2>
-        </div>
-        <ul className="my-3">
-          {listUser.map((user) => (
-            <li key={user.id}>
-              {user.nombre} - {user.apellido}
-              <button className="btn btn-danger mx-2" onClick={()=> deleteData(user.id)}>Delete</button>
-              <button className="btn btn-warning" onClick={()=> modoEditing(user)}>Edit</button>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <section className="my-2">
+            <div className="my-2"><h2>Lista de usuarios</h2></div>
+            <ul>
+              {
+                listUser.map((user)=> (
+                  <li key={user.id}>
+                    {user.nombre} - {user.apellido}
+                    <button className="btn btn-danger mx-2" onClick={()=> deleteData(user.id)}>Eliminar</button>
+                    <button className="btn btn-warning mx-2" onClick={()=> ismodeEdit(user)}>Editar</button>
+                  </li>
+                ))
+              }
+            </ul>
+      </section>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,14 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 const firebaseConfig = {
-    apiKey: "AIzaSyDcEYUbFPlx0vObp0VN5h5QRQL0oQmp5SM",
-    authDomain: "react-firebase-practicav2.firebaseapp.com",
-    projectId: "react-firebase-practicav2",
-    storageBucket: "react-firebase-practicav2.appspot.com",
-    messagingSenderId: "46895030075",
-    appId: "1:46895030075:web:3deca66e240e6a1653e11e",
-    measurementId: "G-L4TVMH3QD0"
-  };
-  
-  // Initialize Firebase
- firebase.initializeApp(firebaseConfig);
- export {firebase}
- 
+  apiKey: "AIzaSyDcEYUbFPlx0vObp0VN5h5QRQL0oQmp5SM",
+  authDomain: "react-firebase-practicav2.firebaseapp.com",
+  projectId: "react-firebase-practicav2",
+  storageBucket: "react-firebase-practicav2.appspot.com",
+  messagingSenderId: "46895030075",
+  appId: "1:46895030075:web:3deca66e240e6a1653e11e",
+  measurementId: "G-L4TVMH3QD0"
+};
+
+firebase.initializeApp(firebaseConfig);
+export {firebase}
